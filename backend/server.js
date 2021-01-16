aca5724a0018e03b285d415ae824130e89ae22ad
Harsh Solanki <hsolanki1884@gmail.com>
2021-01-16 13:44:15 +0530

    backend complete

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+app.post("/", (req, res) => {
+  let time_taken, displacement, speed;
+  
+  const { river_velocity, person_velocity, width ,angle_of_swim} = req.body;
+  
+  if(angle_of_swim  <= 90) {
+    speed = person_velocity + river_velocity;
+    time_taken = width / ((speed)* Math.sin(angle_of_swim*0.0174533));
+  } else{
+    speed = person_velocity - river_velocity
+    time_taken = width / (speed * Math.cos(angle_of_swim*0.0174533));
+  }
+
+  displacement = speed * time_taken;
+  displacement = Math.sqrt((displacement * displacement) -( width * width));
+  console.log(displacement);
+  res.status(200).json({ time_taken, displacement });
+});
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
